ab2f1817f594221237d9150c035efc8c46db0167
marinellavar <mvarenova@gmail.com>
2022-11-06 12:11:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
+
+
 function App() {
+  const [countries, setCountries] = useState (0);
+  const [visit,setVisit] = useState(data);
+  
+  
+  
+  const[showMore, setShowMore] = useState (false);
+  const {id, name, description, location, image} = data[countries];
+  
+  const nextCountry =() =>{
+    setCountries((countries =>{
+      countries ++;
+      if (countries > data.length-1){
+        countries =0;
+      }
+      return countries
+    }))
+  }
+
+  const previousCountry =() =>{
+    setCountries(countries =>{
+      countries--;
+      if (countries < 0){
+       return data.length-1;
+      }
+      return countries;
+    })
+  }
+
+  const removedCountry = (id) =>{
+    let newCountry = visit.filter (visit => visit.id !== id);
+    
+    setVisit(newCountry)
+
+  }
+
+  // {countries.map( element =>{
+  //   const {id, name, description, location, image} = element;
+  // })}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className='container'>
+        <h1>Countries I would like to visit</h1>
+      </div>
+      <div className='container'>
+        <h2>{id} - {name}</h2>
+      </div>
+
+      <div className='container'>
+        <img src ={image} height = '400px' alt='pic'/>
+      </div>
+      
+      <div className='container'>
+        <p>{showMore ? description : description.substring(0,230)+ "..."}
+        <button onClick={() =>setShowMore(!showMore)}>{!showMore ?"Show more" : "Show less" }</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className='container'>
+        <a href={location}>Location</a>
+        
+      </div>
+      
+      <div className='btn container'>
+        <button onClick={previousCountry}>Previous</button>
+        <button onClick={nextCountry}>Next</button>
+
+      </div>
+
+
+      <div className='container'>
+        <button onClick={()=> removedCountry(id)}>Remove</button>
+      </div>
+
+      <div className='container'>
+        <button onClick={() => setVisit([])}>Delete all</button>
+      </div>
+
+      
+
+      
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,109 @@
+
+
+export const data = [
+    {
+        id:1,
+        name: 'Argentina',
+        description:'Argentina (Spanish pronunciation:  officially the Argentine Republic[A] (Spanish: República Argentina), is a country in the southern half of South America. Argentina covers an area of 2,780,400 km2 (1,073,500 sq mi),[B] making it the second-largest country in South America after Brazil, the fourth-largest country in the Americas, and the eighth-largest country in the world. It shares the bulk of the Southern Cone with Chile to the west, and is also bordered by Bolivia and Paraguay to the north, Brazil to the northeast, Uruguay and the South Atlantic Ocean to the east, and the Drake Passage to the south. Argentina is a federal state subdivided into twenty-three provinces, and one autonomous city, which is the federal capital and largest city of the nation, Buenos Aires. The provinces and the capital have their own constitutions,but exist under a federal system. Argentina claims sovereignty over the Falkland Islands, South Georgia and the South Sandwich Islands, and a part of Antarctica.',
+        location:'https://www.google.ru/maps/place/%D0%90%D1%80%D0%B3%D0%B5%D0%BD%D1%82%D0%B8%D0%BD%D0%B0/@-37.0529743,-81.6426551,4z/data=!4m6!3m5!1s0x95bccaf5f5fdc667:0x3d2f77992af00fa8!8m2!3d-38.416097!4d-63.616672!16zL20vMGpnZA',
+        image: 'https://images.unsplash.com/flagged/photo-1576542062543-b11118d96d53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+        showMore: false,
+    },
+
+    {
+        id: 2,
+        name: 'Canada',
+        description:"Canada is a country in North America. Its ten provinces and three territories extend from the Atlantic Ocean to the Pacific Ocean and northward into the Arctic Ocean, covering over 9.98 million square kilometres (3.85 million square miles), making it the world's second-largest country by total area. Its southern and western border with the United States, stretching 8,891 kilometres (5,525 mi), is the world's longest binational land border. Canada's capital is Ottawa, and its three largest metropolitan areas are Toronto, Montreal, and Vancouver.",
+        location:"https://www.google.ru/maps/place/%D0%9A%D0%B0%D0%BD%D0%B0%D0%B4%D0%B0/@50.8246198,-130.131182,3z/data=!3m1!4b1!4m6!3m5!1s0x4b0d03d337cc6ad9:0x9968b72aa2438fa5!8m2!3d56.130366!4d-106.346771!16zL20vMGQwNjBn",
+        image: 'https://images.unsplash.com/photo-1588733103629-b77afe0425ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
+    },
+
+
+    {
+        id:3,
+        name:"China",
+        description:"China,[j] officially the People's Republic of China (PRC),[k] is a country in East Asia. It is the world's most populous country with a population exceeding 1.4 billion people.[l] China spans five geographical time zones[m] and borders fourteen countries by land,[n] the most of any country in the world, tied with Russia. China also has a narrow maritime boundary with the disputed Taiwan.[o][p] Covering an area of approximately 9.6 million square kilometers (3,700,000 sq mi), it is the world's third largest country by total land area.[q] The country consists of 23 provinces,[o] five autonomous regions, four municipalities, and two Special Administrative Regions (Hong Kong and Macau). The national capital is Beijing, and the most populous city and financial center is Shanghai.",
+        location:'https://www.google.ru/maps/place/%D0%9A%D0%B8%D1%82%D0%B0%D0%B9/@34.4140974,86.0474698,4z/data=!3m1!4b1!4m6!3m5!1s0x31508e64e5c642c1:0x951daa7c349f366f!8m2!3d35.86166!4d104.195397!16zL20vMGQwNXcz',
+        image: 'https://images.unsplash.com/photo-1524548209323-6fb4a0d4a4a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80'
+    },
+
+
+    {
+        id:4,
+        name:'India',
+        description:'India, officially the Republic of India (Hindi: Bhārat Gaṇarājya),[25] is a country in South Asia. It is the seventh-largest country by area, the second-most populous country, and the most populous democracy in the world. Bounded by the Indian Ocean on the south, the Arabian Sea on the southwest, and the Bay of Bengal on the southeast, it shares land borders with Pakistan to the west;[f] China, Nepal, and Bhutan to the north; and Bangladesh and Myanmar to the east. In the Indian Ocean, India is in the vicinity of Sri Lanka and the Maldives; its Andaman and Nicobar Islands share a maritime border with Thailand, Myanmar, and Indonesia. The nations capital city is New Delhi.',
+        location:'https://www.google.ru/maps/place/%D0%98%D0%BD%D0%B4%D0%B8%D1%8F/@20.0114083,64.4398422,4z/data=!3m1!4b1!4m6!3m5!1s0x30635ff06b92b791:0xd78c4fa1854213a6!8m2!3d20.593684!4d78.96288!16zL20vMDNyazA',
+        image: 'https://images.unsplash.com/photo-1532664189809-02133fee698d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80'
+    },
+
+
+    {
+        id:5,
+        name:'New Zeland',
+        description:'New Zealand (Māori: Aotearoa [aɔˈtɛaɾɔa]) is an island country in the southwestern Pacific Ocean. It consists of two main landmasses—the North Island (Te Ika-a-Māui) and the South Island (Te Waipounamu)—and over 700 smaller islands. It is the sixth-largest island country by area, covering 268,021 square kilometres (103,500 sq mi). New Zealand is about 2,000 kilometres (1,200 mi) east of Australia across the Tasman Sea and 1,000 kilometres (600 mi) south of the islands of New Caledonia, Fiji, and Tonga. The countrys varied topography and sharp mountain peaks, including the Southern Alps, owe much to tectonic uplift and volcanic eruptions. New Zealands capital city is Wellington, and its most populous city is Auckland.',
+        location:'https://www.google.ru/maps/place/%D0%9D%D0%BE%D0%B2%D0%B0%D1%8F+%D0%97%D0%B5%D0%BB%D0%B0%D0%BD%D0%B4%D0%B8%D1%8F/@-39.5073463,157.0036764,4z/data=!3m1!4b1!4m6!3m5!1s0x6d2c200e17779687:0xb1d618e2756a4733!8m2!3d-40.900557!4d174.885971!16zL20vMGN0d19i',
+        image: 'https://images.unsplash.com/photo-1624589010366-365b61efb41b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
+    },
+
+
+
+    {
+        id:6,
+        name: 'Australia',
+        description:'Australia, officially the Commonwealth of Australia, is a sovereign country comprising the mainland of the Australian continent, the island of Tasmania, and numerous smaller islands.[13] With an area of 7,617,930 square kilometres (2,941,300 sq mi),[14] Australia is the largest country by area in Oceania and the worlds sixth-largest country. Australia is the oldest,[15] flattest,[16] and driest inhabited continent,[17][18] with the least fertile soils.[19][20] It is a megadiverse country, and its size gives it a wide variety of landscapes and climates, with deserts in the centre, tropical rainforests in the north-east, and mountain ranges in the south-east.',
+        location:'https://www.google.ru/maps/place/%D0%90%D0%B2%D1%81%D1%82%D1%80%D0%B0%D0%BB%D0%B8%D1%8F/@-3.5250143,50.6159675,3z/data=!3m1!4b1!4m6!3m5!1s0x2b2bfd076787c5df:0x538267a1955b1352!8m2!3d-25.274398!4d133.775136!16zL20vMGNoZ2h5',
+        image: 'https://images.unsplash.com/photo-1494948141550-221698c089c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80'
+    },
+
+    {
+        id:7,
+        name: 'Tanzania',
+        description:'Tanzania (/ˌtænzəˈniːə/;[9][10][b] Swahili: [tanzaˈni.a]), officially the United Republic of Tanzania (Swahili: Jamhuri ya Muungano wa Tanzania), is a country in East Africa within the African Great Lakes region. It borders Uganda to the north; Kenya to the northeast; Comoro Islands and the Indian Ocean to the east; Mozambique and Malawi to the south; Zambia to the southwest; and Rwanda, Burundi, and the Democratic Republic of the Congo to the west. Mount Kilimanjaro, Africas highest mountain, is in northeastern Tanzania. According to the United Nations, Tanzania has a population of 63.59 million, making it the most populous country located entirely south of the Equator.',
+        location:'https://www.google.ru/maps/place/%D0%A2%D0%B0%D0%BD%D0%B7%D0%B0%D0%BD%D0%B8%D1%8F/@-6.3533502,30.4909746,6z/data=!3m1!4b1!4m6!3m5!1s0x184b51314869a111:0x885a17314bc1c430!8m2!3d-6.369028!4d34.888822!16zL20vMDdkemY',
+        image: 'https://images.unsplash.com/photo-1570463662416-7d8e39fc67e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1854&q=80'
+
+    },
+
+    {
+        id:8,
+        name: 'Austria',
+        description:'Austria,[c] officially the Republic of Austria,[d] is a landlocked country in the southern part of Central Europe, lying in the Eastern Alps. It is a federation of nine states, one of which is the capital, Vienna, the most populous city and state. The country is bordered by Germany to the northwest, the Czech Republic to the north, Slovakia to the northeast, Hungary to the east, Slovenia and Italy to the south, and Switzerland and Liechtenstein to the west. It occupies an area of 83,871 km2 (32,383 sq mi) and has a population of 9 million.[12]',
+        location:'https://www.google.ru/maps/place/%D0%90%D0%B2%D1%81%D1%82%D1%80%D0%B8%D1%8F/@47.6746131,11.1028837,7z/data=!3m1!4b1!4m6!3m5!1s0x476d079b259d2a7f:0x1012d47bdde4c1af!8m2!3d47.516231!4d14.550072!16zL20vMGg3eA',
+        image: 'https://images.unsplash.com/photo-1573167443175-867d91708f97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1881&q=80'
+
+    },
+
+    {
+        id:9,
+        name: 'Netherlands',
+        description:'The Netherlands (Dutch: Nederland [ˈneːdərlɑnt] (listen)), informally Holland,[12][13][14] is a country located in Northwestern Europe with overseas territories in the Caribbean. It is the largest of four constituent countries of the Kingdom of the Netherlands.[15][16][17] The Netherlands consists of twelve provinces; it borders Germany to the east, Belgium to the south, with a North Sea coastline to the north and west. It shares maritime borders with the United Kingdom, Germany and Belgium in the North Sea.[18] The countrys official language is Dutch, with West Frisian as a secondary official language in the province of Friesland.[1] Dutch Low Saxon and Limburgish are recognised regional languages, while Dutch Sign Language, Sinte Romani and Yiddish are recognised non-territorial languages.[1][2] Dutch, English and Papiamento are official in the Caribbean territories.[1]',
+        location:'https://www.google.ru/maps/place/%D0%9D%D0%B8%D0%B4%D0%B5%D1%80%D0%BB%D0%B0%D0%BD%D0%B4%D1%8B/@52.1917328,3.0368114,7z/data=!3m1!4b1!4m6!3m5!1s0x47c609c3db87e4bb:0xb3a175ceffbd0a9f!8m2!3d52.132633!4d5.291266!16zL20vMDU5ajI',
+        image: 'https://images.unsplash.com/photo-1616321741705-e9a4073af35c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1896&q=80'
+
+    },
+
+
+    {
+        id:10,
+        name: 'Bahrain', 
+        description:'Bahrain, officially the Kingdom of Bahrain,[a] is an island country in Western Asia. It is situated on the Persian Gulf, and comprises a small archipelago made up of 50 natural islands and an additional 33 artificial islands, centered on Bahrain Island which makes up around 83 percent of the countrys landmass. Bahrain is situated between Qatar and the northeastern coast of Saudi Arabia, to which it is connected by the King Fahd Causeway. According to the 2020 census, the countrys population numbers 1,501,635, of which 712,362 are Bahraini nationals.[4] Bahrain spans some 760 square kilometres (290 sq mi),[15] and is the third-smallest nation in Asia after the Maldives and Singapore.[16] The capital and largest city is Manama.',
+        location:'https://www.google.ru/maps/place/%D0%91%D0%B0%D1%85%D1%80%D0%B5%D0%B9%D0%BD/@25.9406805,50.307382,10z/data=!3m1!4b1!4m6!3m5!1s0x3e48524e6a47a211:0x2e9450e2dbda1046!8m2!3d26.0667!4d50.5577!16zL20vMDE2MWM',
+        image: 'https://images.unsplash.com/photo-1588172739763-08df442057f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1908&q=80'
+
+    },
+
+    {
+        id:11,
+        name: 'Saudi Arabia',
+        description:'Saudi Arabia,[d] officially the Kingdom of Saudi Arabia (KSA),[e] is a country on the Arabian Peninsula in Western Asia. It has a land area of about 2,150,000 km2 (830,000 sq mi), making it the fifth-largest country in Asia, the second-largest in the Arab world, and the largest in Western Asia. It is bordered by the Red Sea to the west; Jordan, Iraq, and Kuwait to the north; the Persian Gulf, Qatar and the United Arab Emirates to the east; Oman to the southeast; and Yemen to the south. Bahrain is an island country off the east coast. The Gulf of Aqaba in the northwest separates Saudi Arabia from Egypt. Saudi Arabia is the only country with a coastline along both the Red Sea and the Persian Gulf, and most of its terrain consists of arid desert, lowland, steppe, and mountains. Its capital and largest city is Riyadh. The country is home to Mecca and Medina, the two holiest cities in Islam.',
+        location:'https://www.google.ru/maps/place/%D0%A1%D0%B0%D1%83%D0%B4%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%90%D1%80%D0%B0%D0%B2%D0%B8%D1%8F/@23.956012,36.0460317,5z/data=!4m6!3m5!1s0x15e7b33fe7952a41:0x5960504bc21ab69b!8m2!3d23.885942!4d45.079162!16zL20vMDF6MjE1',
+        image: 'https://images.unsplash.com/photo-1592326871020-04f58c1a52f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80'
+
+    },
+]
+
+
+
+
+    
+    
